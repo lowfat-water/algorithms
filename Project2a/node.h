ce207d738b3a5ad5bedef969a8389030539df033
Alina Rossi-Conaway <30272497+alinarc126@users.noreply.github.com>
2018-01-29 12:04:34 -0500

    Add files via upload

---
+++ Project2a/node.h
@@ -0,0 +1,27 @@
+#ifndef NODE_H
+#define NODE_H
+
+#ifndef NULL
+#include <cstddef>
+#endif  // NULL
+
+// linked list node
+template <typename T>
+class node
+{
+   public:
+      T cardInfo;      // data held by the node
+      node<T> *next;    // next node in the list
+
+      // default constructor with no initial value
+      node() : next(NULL)
+      {}
+
+      // constructor. initialize nodeValue and next
+      node(const T& item, node<T> *nextNode = NULL) : 
+			  cardInfo(item), next(nextNode)
+      {}
+    
+};
+
+#endif   // NODE_CLASS
