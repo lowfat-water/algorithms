ec20634984cba1c1ef91e5eb6894abb69b55483a
Dan <bartels.dl>
2018-02-05 04:17:48 -0500

    Broken shuffle function implemented

--- Project2/deck.cpp
+++ Project2/deck.cpp
@@ -17,25 +17,25 @@ deck::deck()
     temp = NULL;
 
     for(int i = 2; i <= 14; i++) //adds nodes to the list that contain cards of suit "Clubs"
-    { 
+    {
         card newCard(i, "Clubs");
         addNode(newCard);
-    }   
+    }
 
     for(int i = 2; i <= 14; i++) //adds nodes to the list that contain cards of suit "Diamonds"
-    { 
+    {
         card newCard(i, "Diamonds");
         addNode(newCard);
-    }  
+    }
 
     for(int i = 2; i <= 14; i++) //adds nodes to the list that contain cards of suit "Hearts"
-    { 
+    {
         card newCard(i, "Hearts");
         addNode(newCard);
     }
 
     for(int i = 2; i <= 14; i++) //adds nodes to the list that contain cards of suit "Spades"
-    { 
+    {
         card newCard(i, "Spades");
         addNode(newCard);
     }
@@ -58,14 +58,14 @@ deck::deck(const deck &sampleDeck)
 void deck::addNode(card &newCard)
 {
     node<card> *n = new node <card>; //dynamically allocates memory for a node n (with data type card) and creates a pointer to it
-    
-    
+
+
     if(n == NULL) //throws error if the heap is out of memory
     {
         throw memoryAllocationError("Memory allocation error for card class");
     }
-    
-    
+
+
     n->next = NULL; //sets this node as the end of the list, with its "next" pointer pointing to NULL
     n->cardInfo = newCard; //stores the information from the argument (newCard) in the node's cardInfo
 
@@ -100,12 +100,12 @@ void deck::printDeck()
 
 card deck::deal()
 {
-    
+
         curr = head;
         head = head->next;
         return curr->cardInfo;
     //cout << " the head is " << head->cardInfo << endl;
-    
+
     //return curr->cardInfo;
     //delete curr;
 }
@@ -121,7 +121,7 @@ void deck::replace(card &sampleCard)
 
 deck & deck::operator = (const deck & sampleDeck)
 {
- 
+
     /*if(sampleDeck.head == NULL) //throws error if the heap is out of memory
     {
         throw rangeError("original deck is empty");
@@ -133,13 +133,13 @@ deck & deck::operator = (const deck & sampleDeck)
 
 
     temp = sampleDeck.head;
-    
+
         while (temp != NULL)
         {
             addNode(temp->cardInfo);
             temp = temp->next;
         }
-    
+
         cout << "deck overloaded assignmwnt operator invoked " << endl;
     return *this;
 }
@@ -154,3 +154,62 @@ deck::~deck()
     }
     cout << "destructor invoked" << endl;
 }
+
+card searchCard (const card & searchCard)
+{
+  long value;
+  long suit;
+  randomNumber r(0); //seed the generator
+
+  value = r.random(2:14); //generates random number (0-3)
+  suit = r.random(4);
+  if (suit == 0)
+  {
+    card newCard (value, "Clubs");
+    searchCard = newCard;
+  }
+
+  if (suit == 1)
+  {
+    card newCard (value, "Diamonds");
+    searchCard = newCard;
+  }
+
+  if (suit == 2)
+  {
+    card newCard (value, "Hearts");
+    searchCard = newCard;
+  }
+
+  if (suit == 3)
+  {
+    card newCard (value, "Spades");
+    searchCard = newCard;
+  }
+  return searchCard;
+}
+
+deck deck::shuffle(deck & sampleDeck)
+{
+  for (i=0, i<52, i++)
+  {
+    card key = searchCard;
+    x = sampleDeck.head;
+    while (x != NULL)
+    {
+      if (curr->nodeValue != key)
+      {
+        prev = curr;
+        curr = next;
+      }
+      prev->next = curr->next;
+      curr = front;
+    }
+  }
+}
+
+/*Sorting implementation:
+ use rand to get a random number 0-3. If the card is 0, its clubs. 1 is diamonds.
+ 2 is hearts. 3 is spades. then randomly choose a number 2-14. Use these to create
+ a card to look through the linked list for. Move that card to the front when it
+ is found. Iterate 52 times. */
