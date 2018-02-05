1aa3b27d3fdaae477474e8dc2729ee494c03f2f6
Alina Rossi-Conaway <alina@Alinas-MacBook-Pro.local>
2018-02-05 13:42:16 -0500

    okay actually done now

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -38,56 +38,53 @@ void playFlip()
     
         cin >> response;
     
-        //if (response == 'Y' || response == 'y')
-        //{
-            card card1 = newDeck.deal();
+        card card1 = newDeck.deal();
 
-            cout << "Your card is " << card1 << endl;
+        cout << "Your card is " << card1 << endl;
 
-            if (card1.getValue() == 14)
-            {
-                cout << "You get 10 points!" << endl;
-                numPoints += 10;
-                cout << "Your new score is " << numPoints << endl;
-            }
+        if (card1.getValue() == 14)
+        {
+            cout << "You get 10 points!" << endl;
+            numPoints += 10;
+            cout << "Your new score is " << numPoints << endl;
+        }
 
-            else if (card1.getValue()== 11 || card1.getValue()== 12 || card1.getValue()== 13)
-            {
-                cout << "You get 5 points!" << endl;
-                numPoints += 5;
-                cout << "Your new score is " << numPoints << endl;
-            }
+        else if (card1.getValue()== 11 || card1.getValue()== 12 || card1.getValue()== 13)
+        {
+            cout << "You get 5 points!" << endl;
+            numPoints += 5;
+            cout << "Your new score is " << numPoints << endl;
+        }
 
-            else if (card1.getValue()== 8 || card1.getValue()== 9 || card1.getValue()== 10)
-            {
-                cout << "You get 0 points" << endl;
-                numPoints += 0;
-                cout << "Your new score is " << numPoints << endl;
-            }
+        else if (card1.getValue()== 8 || card1.getValue()== 9 || card1.getValue()== 10)
+        {
+            cout << "You get 0 points" << endl;
+            numPoints += 0;
+            cout << "Your new score is " << numPoints << endl;
+        }
 
-            else if (card1.getValue()== 7)
-            {
-                cout << "You lose half your points :(" << endl;
-                numPoints = numPoints/2;
-                cout << "Your new score is " << numPoints << endl;
-            }
+        else if (card1.getValue()== 7)
+        {
+            cout << "You lose half your points :(" << endl;
+            numPoints = numPoints/2;
+            cout << "Your new score is " << numPoints << endl;
+        }
 
-            else if (card1.getValue()== 2 || card1.getValue()== 3 || card1.getValue()== 4 || card1.getValue()== 5 || card1.getValue()== 6)
-            {
-                cout << "You lose all your points :(" << endl;
-                numPoints = 0;
-                cout << "Your new score is " << numPoints << endl;
-            }
+        else if (card1.getValue()== 2 || card1.getValue()== 3 || card1.getValue()== 4 || card1.getValue()== 5 || card1.getValue()== 6)
+        {
+            cout << "You lose all your points :(" << endl;
+            numPoints = 0;
+            cout << "Your new score is " << numPoints << endl;
+        }
 
-            if(card1.getSuit() == "Hearts")
-            {
-                cout << "You get one extra point!" << endl;
-                numPoints += 1;
-                cout << "Your new score is " << numPoints << endl;
-            }
-
-            newDeck.replace(card1);
-        //}
+        if(card1.getSuit() == "Hearts")
+        {
+            cout << "You get one extra point!" << endl;
+            numPoints += 1;
+            cout << "Your new score is " << numPoints << endl;
+        }
+        
+        newDeck.replace(card1);
     }
     if (response == 'N' || response == 'n')
     {
