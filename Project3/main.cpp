1504cfdb9afcf59ab25455467c30ce586356aada
Alina Rossi-Conaway <alina@Alinas-MacBook-Pro.local>
2018-02-12 18:37:48 -0500

    wrapping

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -25,36 +25,68 @@ int main()
 void findMatches(wordList &newList, grid &newGrid)
 {   
     int nRows = newGrid.getRows();
+    cout << "nRows is " << nRows << endl;
     int nCols = newGrid.getCols();
-    int m = 0, n = 0;
-    vector <char> arr(1);
+    int m = 9, n = 0;
+    vector <char> arr(0);
     //for (int m = 0; m < nRows; m++) //choosing starting point
     {
       //  for(int n = 0; n < nCols; n++) //choosing starting point
         {
-            for(int i = m; i < nRows; i++) //go south
+            int i = m, j = n;
+            while(arr.size() < nRows) //go south
             {
                 char letter = newGrid.getChar(i, n);
-                arr.resize(i+1);
-                arr.at(i) = letter;
+                arr.resize(arr.size()+1);
+              //  cout << "array size is " << arr.size() << endl;
+                arr.at(arr.size()-1) = letter;
                 string word(arr.begin(), arr.end());
-                cout << "word is " << word << endl;
+             //   cout << "word is " << word << endl;
                 newList.search(word);
+                i++;
+                if(i >= nRows)
+                {
+                    i = i % nRows;
+                }
             }
-            for(int j = n; j < nCols; j++) //go east
+
+            arr.resize(0);
+
+            while(arr.size() < nRows) //go north
+            {
+                char letter = newGrid.getChar(i, n);
+                arr.resize(arr.size()+1);
+              //  cout << "array size is " << arr.size() << endl;
+                arr.at(arr.size()-1) = letter;
+                string word(arr.begin(), arr.end());
+             //   cout << "word is " << word << endl;
+                newList.search(word);
+                i--;
+                if(i < 0)
+                {
+                    i = nRows + i;
+                }
+            }
+
+            arr.resize(0);
+
+            while(arr.size() < nCols) //go east
             {
                 char letter = newGrid.getChar(m, j);
-                arr.resize(j+1);
-                arr.at(j) = letter;
+                arr.resize(arr.size()+1);
+               // cout << "array size is " << arr.size() << endl;
+                arr.at(arr.size()-1) = letter;
                 string word(arr.begin(), arr.end());
-                cout << "word is " << word << endl;
+              //  cout << "word is " << word << endl;
                 newList.search(word);
+                j++;
+                if(j >= nCols)
+                {
+                    j = j % nCols;
+                }
             }
-            for (int j = n; j < nCols; j--) //go west
-            {
-                char letter = newGrid.getChar(m, j);
-                
-            }
+
+            arr.resize(0);
         }
     }
  
