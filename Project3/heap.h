030cfde5b3e5d21f9a300ef9348a70ce5de7fd21
Alina Rossi-Conaway <alina@Alinas-MacBook-Pro.local>
2018-02-16 14:56:00 -0500

    working on heap class

---
+++ Project3/heap.h
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+template <class T>
+class heap
+{
+    public:
+        int parent(int i); //returns index of parent for index i
+        int left(int i); //returns index of left child for index i
+        int right(int i); //returns index of left child for index i
+        int getItem(int n); //returns nth item in the heap
+        void initializeMaxHeap(); //initializes heap to 0s?
+        void maxHeapify(vector <T> &A, int i);
+        void buildMaxHeap(vector <T> &A);
+        void initializeMinHeap(); //initializes heap to 0s?
+        void minHeapify(vector <T> &A, int i);
+        void buildMinHeap(vector <T> &A);
+    private:
+        vector <T> heapVector;
+}
---
+++ Project3/heapify.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+void buildMaxHeap(vector <int> &A);
+void maxHeapify(vector <int> &A, int i);
+void exchange(vector <int> &A, int n, int m);
+int left(int i);
+int right(int i);
+void print(vector <int> &A);
+void heapSort(vector <int> &A);
+
+int main()
+{
+    vector <int> A(11);
+    int array[] = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7, 28};
+    for(int i = 0; i < 11; i++)
+    {
+        A.at(i) = array[i];
+    }
+    print(A);
+    //buildMaxHeap(A);
+    heapSort(A);
+    print(A);
+}
+
+void buildMaxHeap(vector <int> &A)
+{
+    int heapSize = A.size();
+    for (int i =(floor(heapSize/2)); i >= 0; i--)
+    {
+        maxHeapify(A,i);
+    }
+    //print(A);
+    cout << "max heap created." << endl;
+}
+
+void maxHeapify(vector <int> &A, int i)
+{
+    cout << "max heapify for index " << i << endl;
+    int l = left(i);
+    int r = right(i);
+    
+    int largest;
+    if (l < A.size() && A.at(l) > A.at(i))
+        largest = l;
+    else 
+        largest = i;
+    if (r < A.size() && A.at(r) > A.at(largest))
+        largest = r;
+    if (largest != i)
+    {
+        exchange(A, i, largest);
+        maxHeapify(A, largest);
+    }
+    print(A);
+}
+
+void exchange(vector <int> &A, int n, int m)
+{
+        int temp = A.at(n);
+        A.at(n) = A.at(m);
+        A.at(m) = temp;
+        cout << "exchanged items " << n << " and " << m << endl;
+}
+
+int left(int i)
+{
+    return (2*i+1);
+}
+
+int right(int i)
+{
+    return (2*i+2);
+}
+
+void print(vector <int> &A)
+{
+    for(int j = 0; j < A.size(); j++)
+        cout << A.at(j) << " ";
+    cout << endl;
+}
+
+void heapSort(vector <int> &A)
+{
+    int heapSize = A.size();
+    buildMaxHeap(A);
+
+    for(int i = A.size()-1; i > 2; i--)
+    {
+        exchange(A, 0, i);
+        heapSize = heapSize - 1;
+        maxHeapify(A, 0);
+    }
+}
