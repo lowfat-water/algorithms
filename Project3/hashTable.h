9a7bf7b984be46d22a91808195f41f9032d8637d
Alina Rossi-Conaway <alina@Alinas-MacBook-Pro.local>
2018-02-19 16:40:04 -0500

    hash constructor

--- Project3/hashTable.h
+++ Project3/hashTable.h
@@ -16,6 +16,7 @@ class hashTable
         void inList(T &item);
         int hash(T &item);
         void resize(int numRows, int numCols);
+        void printVector(int index);
 
 
     private:
@@ -26,36 +27,45 @@ class hashTable
 template <typename T>
 hashTable<T>::hashTable(vector <T> newVector)
 {
-    nSlots = newVector.size()/3;
+    int nItems = newVector.size();
+    nSlots = nItems/3;
     table.resize(nSlots);
+    for(int i = 0; i < nItems; i++)
+    {
+        int index = hash(newVector.at(i));
+        addItem(newVector.at(i));
+        cout << "added item  " << newVector.at(i) << " to table at index " << index << endl;
+    }
 }
 
 template <typename T>
 void hashTable<T>::addItem(T &item)
 {
     int hashKey = hash(item);
+    if (hashKey < nSlots)
+    {
+        table[hashKey].push_back(item);
+    }
 }
 
 template <typename T>
 int hashTable<T>::hash(T &item)
 {
-    hash <T> h;
-    const size_t value = h(item);
-    return value % nSlots;
+    std::hash<T> h;
+    return h(item) % nSlots;
 }
-/*
+
 template <typename T>
-void hashTable<T>::resize(int numRows, int numCols)
+void hashTable <T>::printVector(int index)
 {
-    int i;
-
-    if (numRows == nSlots && numCols == nCols)
-        return;
-    
-    nSlots = numRows;
-    nCols = numCols;
-
-    hashTable.resize(nSlots);
+    if(index < nSlots)
+    cout << "printing the values that have the key index " << index << endl;
+    int size = table[index].size();
+    for(int i = 0; i < size; i++)
+    {
+        cout << table[index].at(i) << " ";
+    }
+    cout << endl;
 }
-*/
+
 #endif
--- Project3/main2.cpp
+++ Project3/main2.cpp
@@ -10,6 +10,25 @@ int main()
     int arrSize = sizeof(arr)/sizeof(arr[1]);
     vector <int> A(arr, arr + arrSize);
     hashTable<int> newTable(A);
+    newTable.printVector(1);
 
 
+    string arrString[] = {"cat", "mouse", "bear", "zebra", "elephant", "lion", "eagle", "hippopotomus","lemur", "saggitarius", "capricorn"};
+    int arrSize1 = sizeof(arrString)/sizeof(arrString[1]);
+    vector <string> B(arrString, arrString+arrSize1);
+    hashTable <string> strTable(B);
+    for(int i = 0; i < arrSize1/3; i++)
+        strTable.printVector(i);
+
+    int item = 3000;
+
+    hash<int> intHash;
+    hash<string> strHash;
+    size_t value1 = strHash(arrString[0]) % 3;
+    
+    size_t value = intHash(item) % 222;
+
+    cout << "the hash value for " << item << " is " << value << endl;
+    cout << "the hash value for " << arrString[1] << " is " << value1 << endl;
+    
 }
