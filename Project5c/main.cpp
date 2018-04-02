3b562edc0145b5d570015a1d832650447317beda
Alina Rossi-Conaway <alina@Alinas-MacBook-Pro.local>
2018-04-01 22:32:02 -0400

    dijkstra

--- Project5c/main.cpp
+++ Project5c/main.cpp
@@ -78,15 +78,15 @@ void initializeGraph(Graph &g,
     }
 }
 
-void initSingleSource(Graph &g, int w)
+void initSingleSource(Graph &g, Vertex start)
 // Set all node weights to w, except source node
 {
   pair <vertex_iterator, vertex_iterator> vItrRange = vertices(g);
   for (vertex_iterator vItr = vItrRange.first; vItr != vItrRange.second; ++vItr)
   {
-    if (vItr == vItrRange.first)
+    if (*vItr == start)
       g[*vItr].weight = 0; //Init-Single-Source: sets source node's weight to 0
-    else g[*vItr].weight = w;
+    else g[*vItr].weight = LargeValue;
   }
 }
 
@@ -105,7 +105,7 @@ void relax(Graph &g, Vertex u, Vertex v)
 
 bool bellmanFord(Graph &g, Vertex s, Vertex goal, stack<Vertex> &moves)
 {
-  initSingleSource(g, LargeValue);
+  initSingleSource(g, s);
   pair <edge_iterator, edge_iterator> eItrRange = edges(g);
 
   int size = num_vertices(g);
@@ -121,8 +121,6 @@ bool bellmanFord(Graph &g, Vertex s, Vertex goal, stack<Vertex> &moves)
         cout << "vertex v is " << v << endl << endl;
         relax(g, u, v);
     }
-
-
   }
 
   for(edge_iterator eItr = eItrRange.first; eItr != eItrRange.second; ++eItr)
@@ -142,6 +140,53 @@ bool bellmanFord(Graph &g, Vertex s, Vertex goal, stack<Vertex> &moves)
   }
   moves.push(s);
   return true;
+}
+
+bool dijkstra(Graph &g, Vertex start, Vertex goal, stack<Vertex> &moves)
+{
+    initSingleSource(g, start);
+
+    stack <Vertex> s;
+    vector <Vertex> v;
+
+    pair <vertex_iterator, vertex_iterator> vItrRange = vertices(g);
+    for (vertex_iterator vItr = vItrRange.first; vItr != vItrRange.second; ++vItr)
+    {
+        v.push_back(*vItr);
+    }
+
+    heapV<Vertex, Graph> pq;
+    pq.initializeMinHeap(v, g);
+
+/*     cout << pq << endl;
+    return true; */
+
+    while(pq.size() != 0)
+    {
+        Vertex u = pq.extractMinHeapMinimum(g);
+        s.push(u);
+        pair <adj_iterator, adj_iterator> adjV = adjacent_vertices(u, g);
+        for(adj_iterator vItr = adjV.first; vItr != adjV.second; ++vItr)
+        {
+            pair <Edge, bool> newEdge = edge(u, *vItr, g);
+            Edge e = newEdge.first;
+            if(g[e].weight < 0)
+                return false;
+            else relax(g, u, *vItr);
+        }
+    }
+    //cout << pq << endl;
+
+    Vertex curr = goal;
+    while(curr != start)
+    {
+      moves.push(curr);
+      curr = g[curr].pred;
+    }
+    moves.push(start);
+
+    return true;
+
 }
 
 void printPath(stack <Vertex> &s, Graph &g)
@@ -165,7 +210,7 @@ int main()
     {
         ifstream fin;
 
-        string fileName = "graph/graph2.txt";
+        string fileName = "graph/graph7.txt";
 
         fin.open(fileName.c_str());
         if(!fin)
@@ -186,21 +231,22 @@ int main()
         int num = num_vertices(g);
         cout << "This graph has " << num << " vertices" << endl;
 
-        heapV <Vertex, Graph> pq;
-        pair <vertex_iterator, vertex_iterator> vItrRange = vertices(g);
-        vector <Vertex> list1;
-        for(vertex_iterator vItr = vItrRange.first; vItr != vItrRange.second; ++vItr)
+         stack <Vertex> moves;
+/*        if (!bellmanFord(g, start, end, moves))
+            cout << "Negative cycles reachable from source." << endl;
+        else 
         {
-            list1.push_back(*vItr);
-        }
-        pq.initializeMinHeap(list1, g);
-
-        cout << pq << endl;
-
-        stack <Vertex> moves;
-        bellmanFord(g, start, end, moves);
-
-        printPath(moves, g);        
+            cout << "Shortest path: " << endl;
+            printPath(moves, g);
+        } */
+        
+        if (!dijkstra(g, start, end, moves))
+            cout << "Negative edge weights. Use Bellman-Ford for this graph." << endl;
+        else 
+        {
+            cout << "Shortest path: " << endl;
+            printPath(moves, g);
+        }    
     }
     catch(const std::exception& e)
     {
